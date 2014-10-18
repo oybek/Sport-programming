51050b43b8fabc35c81c0b47e547db722f99737c
oybek <aybek_hashimov@mail.ru>
2014-10-19 02:07:05 +0600

    programming trainings

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 programming-trainings
 =====================
+
+My training program on this sites
+http://acm.timus.ru
+http://train.usaco.org
+http://uva.onlinejudge.org
+
--- uva/11254.cpp
+++ uva/11254.cpp
@@ -6,6 +6,32 @@
 using namespace std;
 
 int main() {
+	int n;
+
+	while (1) {
+		cin >> n;
+		if (n == -1)
+			break;
+
+		int a = n, d = 1, max_d = 1;
+		while (1) {
+			int x = 2*n + d - d*d;
+			int y = 2*d;
+
+			if (x < 0)
+				break;
+
+			if (x > 0 &&
+				x%y == 0) {
+				a = x/y;
+				max_d = max(max_d, d);
+			}
+
+			++d;
+		}
+
+		cout << n << " = " << a << " + ... + " << a + max_d-1 << endl;
+	}
 
 	return 0;
 }
