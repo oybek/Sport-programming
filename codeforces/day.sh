4cf3e7c08ab8bbe3e58625d42455b78c4797e793
o.khashimov <o.khashimov@eastwind.ru>
2017-05-10 16:17:22 +0500

    Day is now more clever

--- codeforces/day.sh
+++ codeforces/day.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "(~˘▾˘)~"
-echo "today you solved $(stat -c %y *.cpp | grep -c $(date +%Y-%m-%d) ) problems"
-echo "yesterday you solved $(stat -c %y *.cpp | grep -c $(date +%Y-%m-%d -d '1 day ago') ) problems"
-echo "2 days ago you solved $(stat -c %y *.cpp | grep -c $(date +%Y-%m-%d -d '2 days ago') ) problems"
+ufiles=$(git ls-files --others --exclude-standard | grep cpp)
+if [[ ${ufiles} =~ .*\.cpp ]];
+then
+	echo "(~˘▾˘)~ today you solved $(stat -c %y ${ufiles} | grep -c $(date +%Y-%m-%d) )"
+else
+	echo "(~˘▾˘)~ today you solved 0"
+fi
 
