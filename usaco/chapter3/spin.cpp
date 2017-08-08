66971c289a434d22940b81d70a7c97a1a1c7d07d
o.khashimov <o.khashimov@eastwind.ru>
2017-08-08 19:19:50 +0500

    solved usaco 'spin' problem

--- usaco/chapter3/spin.cpp
+++ usaco/chapter3/spin.cpp
@@ -8,77 +8,21 @@ LANG: C++
 #include <bits/stdc++.h>
 using namespace std;
 
-template< int SIZE >
-class bit_array
-{
-	private:
-		bitset< SIZE > a;
-
-	public:
-		void reset_range( int i, int n )
-		{
-			while( n-- )
-			{
-				a[ i ] = 0;
-				if( ++i >= SIZE )
-					i = 0;
-			}
-			a[ i ] = 0;
-		}
-
-		void set() { a.set(); }
-		bool all() {
-			for( int i = 0; i < a.size(); ++i )
-				if( a[i] == false )
-					return false;
-			return true;
-		}
-
-#ifdef LOCAL
-		void print() { cout << a.to_string() << endl; }
-#endif
-};
-
-struct wheel
-{
-	int speed; // degrees per second
-	vector< pair<int, int> > wedge;
-};
-
-bit_array<360> circle;
-vector< wheel > w( 5 );
-
-bool is_solution()
-{
-	circle.set();
-
-	for( int i = 0; i < w.size(); ++i )
-	{
-		for( int j = 0; j < w[i].wedge.size(); ++j )
-		{
-			circle.reset_range(
-				w[i].wedge[j].first, 
-				w[i].wedge[j].second 
-			);
-		}
-	}
-
-#ifdef LOCAL
-	circle.print();
-	cout << circle.all() << endl;
-#endif
-
-	return !( circle.all() );
-}
+int circle[360];
+int speed[5];
+vector< pair<int, int> > w[ 5 ];
 
 void rotate()
 {
-	for( int i = 0; i < w.size(); ++i )
+	for( int i = 0; i < 5; ++i )
 	{
-		for( int j = 0; j < w[i].wedge.size(); ++j )
+		for( int j = 0; j < w[i].size(); ++j )
 		{
-			w[i].wedge[j].first += w[i].speed;
-			w[i].wedge[j].first %= 360;
+			w[i][j].first += speed[i];
+			w[i][j].first %= 360;
+
+			w[i][j].second += speed[i];
+			w[i][j].second %= 360;
 		}
 	}
 }
@@ -91,32 +35,45 @@ int main()
 #endif
 
 	//
-	for( int i = 0; i < w.size(); ++i )
+	vector< pair<int, int> > w[ 5 ];
+	for( int i = 0; i < 5; ++i )
 	{
-		cin >> w[i].speed;
+		cin >> speed[i];
 
-		int size;
-		cin >> size;
-
-		w[i].wedge.resize( size );
-		for( int j = 0; j < w[i].wedge.size(); ++j )
-			cin >> w[i].wedge[j].first >> w[i].wedge[j].second;
+		int n;
+		cin >> n;
+		while( n-- )
+		{
+			int x, y;
+			cin >> x >> y;
+			w[i].push_back( make_pair( x, y ) );
+		}
 	}
 
 	//
-	for( int ans = 0; ans < 1000; ++ans )
+	for( int t = 0; t < 10000; ++t )
 	{
-		if( is_solution() )
-		{
-			cout << ans << endl;
-			return 0;
-		}
+		fill( circle, circle+360, 0 );
 
+		for( int i = 0; i < 5; ++i )
+		{
+			for( int j = 0; j < w[i].size(); ++j )
+			{
+				int k = ( w[i][j].first + speed[i] * t )%360;
+				for( int x = 0; x <= w[i][j].second; ++x )
+				{
+					if( ++circle[ (k+x)%360 ] >= 5 )
+					{
+						cout << t << endl;
+						return 0;
+					}
+				}
+			}
+		}
 		rotate();
 	}
 
 	cout << "none" << endl;
-
 	return 0;
 }
 
